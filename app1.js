f04e63ad72b1e4554bb9135a662cc4514bd93e57
Jorge <40173724+jaco9419@users.noreply.github.com>
2020-07-03 13:39:22 -0300

    Create app1.js

---
+++ app1.js
@@ -0,0 +1,11 @@
+//Retrieving All Rows
+
+const { printQueryResults } = require('./utils');
+const sqlite = require('sqlite3');
+
+const db = new sqlite.Database('./db.sqlite');
+
+// Your code below:
+db.all('SELECT * FROM TemperatureData WHERE year = 1970', (error, rows) => {
+  printQueryResults(rows);
+});
