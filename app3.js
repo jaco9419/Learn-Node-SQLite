c0244b2ab24cfc1b253f8ca99426ed75f8486c3d
Jorge <40173724+jaco9419@users.noreply.github.com>
2020-07-03 14:03:42 -0300

    Create app3.js

---
+++ app3.js
@@ -0,0 +1,19 @@
+//Using Placeholders
+
+const { printQueryResults } = require('./utils');
+const sqlite = require('sqlite3');
+
+const db = new sqlite.Database('./db.sqlite');
+
+const ids = [1, 25, 45, 100, 360, 382];
+// your code below:
+ids.forEach(id => {
+  console.log(id);
+  db.get('SELECT * FROM TemperatureData WHERE id = $id',
+  {
+    $id: id
+  }, 
+  (error, row) => {
+    printQueryResults(row);
+  })
+});
